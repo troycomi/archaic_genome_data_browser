0940d8d0a55a4c5db3b73a15ff643bfcce690dcd
Lance Parsons <lparsons@princeton.edu>
2018-10-16 13:53:20 -0400

    Update to click 7 compatible command names

--- data/load_sample_data_tucci2018.sh
+++ data/load_sample_data_tucci2018.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-flask load_data data_source data/tucci2018/tucci2018_data_source.yml
+flask load-data data-source data/tucci2018/tucci2018_data_source.yml
 
-flask load_data superpopulations data/tucci2018/geographic_regions.tsv "Tucci (Unpublished, 2018)"
+flask load-data superpopulations data/tucci2018/geographic_regions.tsv "Tucci (Unpublished, 2018)"
 
-flask load_data populations data/tucci2018/populations.tsv "Tucci (Unpublished, 2018)"
+flask load-data populations data/tucci2018/populations.tsv "Tucci (Unpublished, 2018)"
 
-flask load_data samples data/tucci2018/samples.tsv "Tucci (Unpublished, 2018)"
+flask load-data samples data/tucci2018/samples.tsv "Tucci (Unpublished, 2018)"
